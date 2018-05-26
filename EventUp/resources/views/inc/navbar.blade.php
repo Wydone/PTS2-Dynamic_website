f938d0bed6209f9c95059baf43dafce408376a3a
Clément Coulon <cle.coulon@gmail.com>
2018-05-26 18:29:03 +0200

    first commit

---
+++ EventUp/resources/views/inc/navbar.blade.php
@@ -0,0 +1,26 @@
+<header>
+
+  <!-- Navigation -->
+  <nav class="nav-top">
+    <div class="Menu">
+      <ul>
+
+        <li>
+          <a href="/">ACTUALITES</a>
+        </li>
+        <li>
+          <a href="/calendrier">CALENDRIER</a>
+        </li>
+        <li>
+          <a href="/contact">CONTACT</a>
+        </li>
+        <li>
+          <a href="/moncompte">MON COMPTE</a>
+        </li>
+          <input class="inputSearch" type="text" placeholder="Search" aria-label="Search">
+          <button class="btnSearch" type="submit" >Search</button>
+          <button class="btnConnexion" type="submit" >connexion</button>
+      </ul>
+    </div>
+  </nav>
+</header>
---
+++ EventUp/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+  	<img class="ico-fb"  src="/img/facebook.png" alt="facebook icon">
+
+@endsection
